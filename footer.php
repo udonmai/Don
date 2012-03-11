fe85a70b2fa12aea82d3d5dc83dbee84dc378e78
udonmai <udonmai@gmail.com>
2012-03-11 11:07:17 +0800

    preparing

---
+++ footer.php
@@ -0,0 +1,13 @@
+
+	<div id="footer">
+		<span id="generator-link"><a href="http://wordpress.org/" title="<?php _e( 'WordPress', 'sandbox' ) ?>" rel="generator"><?php _e( 'WordPress', 'sandbox' ) ?></a></span>
+		<span class="meta-sep">|</span>
+		<span id="theme-link"><a href="http://www.plaintxt.org/themes/sandbox/" title="<?php _e( 'Sandbox theme for WordPress', 'sandbox' ) ?>" rel="designer"><?php _e( 'Sandbox', 'sandbox' ) ?></a></span>
+	</div><!-- #footer -->
+
+</div><!-- #wrapper .hfeed -->
+
+<?php wp_footer() ?>
+
+</body>
+</html>
